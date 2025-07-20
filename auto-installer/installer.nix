afda3b0a5d5fcbc13cdb6a135f64247f553b4b72
beatzaplenty <wayne.bennett@live.com>
2025-07-21 00:25:39 +1000

    modified:   auto-installer/installer.nix

--- auto-installer/installer.nix
+++ auto-installer/installer.nix
@@ -2,7 +2,7 @@
 
 {
   imports = [
-    modulesPath + "/installer/cd-dvd/installation-cd-minimal.nix"
+    "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix"
   ];
 
   networking.useDHCP = lib.mkDefault true;
