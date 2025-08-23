dc358a1964ec9a299ad27f3e42156a0db414a09b
beatzaplenty <wayne.bennett@live.com>
2025-08-23 13:02:26 +1000

    modified:   common/home.nix

--- common/home.nix
+++ common/home.nix
@@ -17,14 +17,14 @@ in {
   
 programs.bash.enable = true;
   
-  sops.secrets.nix_conf = {
-    sopsFile = ../secrets/nix.conf.sops;
+  sops.secrets.github-token = {
+    sopsFile = ../secrets/github-token.sops;
     owner = config.home.username;
     mode = "0400";
   };
 
   home.file = {
-    ".config/nix/nix.conf".source = config.sops.secrets.nix_conf.path;
+    ".config/nix/nix.conf".text = ''access-tokens = github.com=''config.sops.secrets.github-token.path
   };
 
   # Optional: packages
